6acd29716d4bf9ef0c6e1534f77121b2853a5111
anangfirmansyah <anangfirmansyahp5@gmail.com>
2024-02-09 17:34:18 +0800

    add: sidebar

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,7 +1,11 @@
+import List from "./list";
+import NewButton from "./new-button";
+
 export const Sidebar = () => {
   return (
     <aside className="fixed text-white z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4">
-      Sidebar
+      <List />
+      <NewButton />
     </aside>
   );
 };
