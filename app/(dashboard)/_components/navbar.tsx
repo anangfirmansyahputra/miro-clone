7f18a8516923fe3cd65ff00615acf4b28e983c9c
anangfirmansyah <anangfirmansyahp5@gmail.com>
2024-02-10 09:57:21 +0800

    add: navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,47 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from "@clerk/nextjs";
+import SearchInput from "./search-input";
+import { useDebounce } from "usehooks-ts";
+import { useRouter } from "next/navigation";
+import InviteButton from "./invite-button";
 
 export default function Navbar() {
+  const { organization } = useOrganization();
+
   return (
     <div className="flex items-center gap-x-4 p-5">
-      <div className="hidden lg:flex lg:flex-1">Search</div>
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth: "376px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                border: "1px solid #e5e7eb",
+                justifyContent: "space-between",
+                backgroundColor: "white",
+              },
+            },
+          }}
+        />
+      </div>
+      {organization && <InviteButton />}
       <UserButton />
     </div>
   );
